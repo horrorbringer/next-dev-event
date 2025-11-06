d3b823b5b5a12c630c7fdaf1b2cd884264c5b836
horrorbringer <vannysmilekh@gmail.com>
2025-11-06 20:00:10 +0700

    add navbar and explore button

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import PrismaticBurst from "@/components/PrismaticBurst";
 import React from "react";
+import NavBar from "@/components/NavBar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,6 +30,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen relative`}
       >
+      <NavBar/>
       <div className="absolute z-[-1] inset-0 top-0 w-full min-h-screen">
           <PrismaticBurst
               animationType="rotate3d"
---
+++ components/ExploreBtn.tsx
@@ -0,0 +1,19 @@
+"use client";
+import Image from "next/image";
+
+const ExploreBtn = () => {
+    return (
+        <button type="button"
+                id="explore-btn"
+                className="mt-7 mx-auto"
+            onClick={()=>console.log("click")}>
+            ExploreBtn
+            <a href="#events">
+                <Image src="/icons/arrow-down.svg"
+                       alt="arrow-downt"
+                       width="24" height="24"/>
+            </a>
+        </button>
+    )
+}
+export default ExploreBtn;
